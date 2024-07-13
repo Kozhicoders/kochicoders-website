13e47b3c5e428952548770efd81ef66a8ee1a317
Sanu <sanukrishnansb@gmail.com>
2024-07-13 15:44:55 +0530

    Sample page added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## How to run 
 npm run docs:dev
+
+## How to add a new page
+Refer `.vuepress/sample.md` file
--- docs/.vuepress/components/Footer.vue
+++ docs/.vuepress/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center p-4 bg-primary/10 mt-16">
-        <p>&copy; {{ new Date().getFullYear() }} <span class="text-primary">Kozhikoders</span></p>
+        <p>&copy; {{ new Date().getFullYear() }} <span class="text-primary">Kozhicoders</span></p>
     </div>
 </template>
 
