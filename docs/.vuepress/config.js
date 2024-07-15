40bdc8b1b3162ea18b91108b27e7c02e36a00e80
Sanu <38312605+sanu-krishnan@users.noreply.github.com>
2024-07-15 11:02:49 +0530

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
+  base: '/kochicoders-website/'
 
   title: '',
   description: 'Technology from the shores of Malabar',
